f42709b459cde5fa8d4838f7b90c4715a3716eaa
abusaidshabib <abusaidshabib712@gmail.com>
2023-12-16 22:57:03 +0600

    done

--- GeekyShows/Numpy/day1.py
+++
@@ -1,4 +0,0 @@
-# In Python, Numpy is a package which contains classes, functions, variables, large library of mathematical functions etc to work with scientific calculation. Numpy can be used to create n dimensional arrays where n is any integer. We can create 1 dimensional array, 2 dimensional array, 3 dimensional array and so on.
-"""
-Numpy's array class is called ndarray. It is also known by alias name array. There is another class array in python which is different from numpy's array class.
-"""
---
+++ GeekyShowsSpecific/DataType.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "mango", "orange"]
+print(type(fruits))
+
+numbers = (1, 2, 3)
+print(type(numbers))
+# print(numbers[1])
+
+alphabets = {'a':'apple', 'b':'ball', 'c':'cat'}
+print(type(alphabets))
+# print(alphabets['a'])
+
+vowels = {'a', 'e', 'i', 'o', 'u'}
+print(type(vowels))
